5ddb89999ec554ff017f5e273c2870bcf643dc92
Ishau <ishau@Ishaus-MacBook-Pro.local>
2021-02-19 18:41:35 -0600

    completed

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,11 +3,41 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++
+}
 
 // Using `console.log` log all the evan values from 1 to 10.
 
+let i=1;
+while(i<=10){
+    if(i%2==0){
+    console.log(i);
+    }
+    i++
+}
+
 // Using `console.log` log all the od values from 1 to 10.
-
+let i=1;
+while(i<=10){
+    if(i%2!==0){
+    console.log(i);
+    }
+    i++
+}
 // Calculate the sum of all numbers from 1 to 10.
-
+let i=1;
+let sum=0;
+while(i<=10){
+    sum=sum+i;
+    i++
+}
+console.log(sum);
 // Log all the values from 1 to 10 using while loop
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++
+}
